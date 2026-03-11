949d3f9db1f373c9dd89aa3c36460b2b0190cc0d
Joshua Laughner <jlaugh@caltech.edu>
2026-03-11 11:37:31 -0700

    Updated VMR benchmark files, gfortran compiler flags, and netCDF commit
    - The previous VMR benchmark files were missing the O2 global DMF that we
      are including for future use
    - The default compiler flags are now appropriate for gfortran v8.5 and similar
      era compilers. An additional set of flags for gfortran v10 and up has been
      added as `install/.compiler_gfortran_modern`
    - The netCDF repo was updated to a new commit that includes (1) a fix for
      expanding AKs to one-per-spectrum when there are NaNs in the slant Xgas
      values and (2) an option to `concat_netcdf` to override the requirement
      that non-concatenated variables be the same across all files for a specific
      list of variables.

--- install/clone_netcdf_writer.sh
+++ install/clone_netcdf_writer.sh
@@ -38,7 +38,7 @@ else
 fi
 
 cd $srcdir
-last_commit_for_ggg="e8200a2f"
+last_commit_for_ggg="f9bdbbb"
 git checkout main >/dev/null 2>/dev/null
 git rev-parse --verify "ggg" &> /dev/null
 if [ $? == 0 ] ; then
